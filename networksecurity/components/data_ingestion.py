8e28226b2b39281833bb2e2833b4089d4d2d7a82
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-07-11 22:12:54 +0530

    Data Validation Completed

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -30,6 +30,7 @@ class DataIngestion:
             collection=self.mongo_client[database_name][collection_name]
 
             df=pd.DataFrame(list(collection.find()))
+
             if "_id" in df.columns.to_list():
                 df=df.drop(columns=["_id"])
 
